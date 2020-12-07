6daef9fd56cc9d75c1843b9d7e1961bf37196b28
PRAKASH <38276550+ThePrakashKumar@users.noreply.github.com>
2020-12-07 23:47:00 +0530

    done

--- app.js
+++ app.js
@@ -2,12 +2,13 @@
 let textArea=document.querySelector("#form-txt-input");
 let button=document.querySelector("#form-btn-translate");
 let outputDiv=document.querySelector("#output-div");
+let twitter=document.querySelector("#output-twitter");
 let whatsapp=document.querySelector("#output-whatsapp");
 
 
-// let serverURL="https://api.funtranslations.com/translate/minion.json";
+let serverURL="https://api.funtranslations.com/translate/dothraki.json";
 
-let serverURL="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+// let serverURL="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 
 // Take input from textarea and make url to make request
 function getTranslationgURL(text){
@@ -25,17 +26,20 @@ function clickHandler(){
     let inputText=textArea.value;
     fetch(getTranslationgURL(inputText))
         .then(respone => respone.json())
-        .then(json => outputDiv.textContent=json.contents.text)
+        .then(json => outputDiv.textContent=json.contents.translated)
         .catch(errorHandler);
 }
 
 // Add event listner to the button
 button.addEventListener("click", clickHandler);
 
-function changeLoc(){
-    location.ancestorOrig
+// Share on Twitter
+function shareTwitter(){
+    outputDiv.innerText ? window.open(`https://twitter.com/intent/tweet?text=${outputDiv.innerText}&hashtags=dothraki`) : alert("Please Translate First!");
 }
+twitter.addEventListener("click", shareTwitter);
 
-
-
-// Whatsapp`https://api.whatsapp.com/send?text=${this.state.quote}`
+function shareWhatsapp(){
+    outputDiv.innerText ? window.open(`https://api.whatsapp.com/send?text=${outputDiv.textContent}`) : alert("Please Translate First!");
+}
+whatsapp.addEventListener("click", shareWhatsapp);
